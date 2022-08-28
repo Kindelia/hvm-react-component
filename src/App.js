666cdd8933d69ab92447dbb33f4267af9c733600
Victor Maia <victor.taelin@gmail.com>
2022-08-28 18:24:14 -0300

    Updates

--- src/App.js
+++ src/App.js
@@ -1,173 +1,201 @@
 import logo from './logo.svg';
 import './App.css';
 import React from "react";
-import { useEffect, useState } from "react";
 
 import hvm from "hvm-js";
 
-const DEFAULT_CODE = `
-  // Main : (App U60)
-  (Main) =
-    let init = 0;
-    let draw = @s
-      let style = [
-        (Pair.new "background" "#F0F0F0")
-        (Pair.new "height" "100%")
-        (Pair.new "display" "flex")
-        (Pair.new "fontSize" "32px")
-        (Pair.new "fontFamily" "monaco")
-        (Pair.new "justifyContent" "center")
-        (Pair.new "alignItems" "center")
-      ]
-      (App.DOM.node "div" (List.nil) style
-        (List.cons (Bool.if (U60.equal s 0) (App.DOM.text "tic") (App.DOM.text "tac"))
-        (List.nil)));
-    let when = @event @state
-      //(IO.do_input @name
-      //(IO.do_output name @cont
-      (IO.pure (- 1 state))
-      //));
-    (App.new init draw when)
+// We're using React.Component, because pure components are terrible for state
+// management, and `useEffect()` is called more than once in ill-specified ways.
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props);
+    this.runtime = null;
+    this.state = {
+      root: null,
+    }
+  }
 
-  // U60.equal (a: U60) (b: U60) : (Bool)
-  (U60.equal a b) = (U60.to_bool (== a b))
+  async componentDidMount() {
+    if (!this.props.code || !this.props.main) {
+      throw "Requires 'main' a 'code' props."
+    }
 
-  // U60.to_bool (n: U60) : (Bool)
-  (U60.to_bool 0) = (Bool.false)
-  (U60.to_bool n) = (Bool.true)
+    // Starts HVM's runtime with the given code
+    this.runtime    = await hvm(this.props.code + EXTRA_CODE(this.props.main));
+    this.MAIN_INIT  = this.runtime.get_id("Main.init");
+    this.MAIN_DRAW  = this.runtime.get_id("Main.draw");
+    this.MAIN_WHEN  = this.runtime.get_id("Main.when");
+    this.PAIR_NEW   = this.runtime.get_id("Pair.new");
+    this.MAYBE_SOME = this.runtime.get_id("Maybe.some");
+    this.MAYBE_NONE = this.runtime.get_id("Maybe.none");
+    this.state.root = this.runtime.create_fun(this.MAIN_INIT, [])
+    this.forceUpdate();
 
-  // IO.pure -(a: Type) (val: a) : (IO a)
-  (IO.pure val) = (IO.done val)
+    // krame event
+    this.run_event("init", []);
+    this.timer = setInterval(() => this.run_event("frame", []), 1000);
 
-  // Bool.if -(a: Type) (b: (Bool)) (t: a) (f: a) : a
-  (Bool.if (Bool.true) t f)  = t
-  (Bool.if (Bool.false) t f) = f
-`;
+    // key_down event
+    document.body.addEventListener("keydown", e => {
+      var key_code = {$: "Num", numb: BigInt(e.keyCode)};
+      this.run_event("key_down", [key_code]);
+    });
 
-const EXTRA_CODE = `
-  // Accessors
-  (App.init (App.new i d w)) = i
-  (App.draw (App.new i d w)) = d
-  (App.when (App.new i d w)) = w
-  (Main.init)                = (App.init Main)
-  (Main.draw s)              = (Pair.new s ((App.draw Main) s))
-  (Main.when e s)            = ((App.when Main) e s)
+    // key_up event
+    document.body.addEventListener("keyup", e => {
+      var key_code = {$: "Num", numb: BigInt(e.keyCode)};
+      this.run_event("key_up", [key_code]);
+    });
 
-  // Must refer all possible events, so that their ids are allocated
-  // Alternative: pre-set their id on HVM, like IO actions
-  // Alternative: make HVM create a new id if user asks an undefined name
-  Load = (App.Event.frame) 
-`;
+    // mouse_down event
+    document.body.addEventListener("mousedown", e => {
+      var button = {$: "Num", numb: BigInt(e.button)};
+      var mouse_pos = {$: "Ctr", name: "Pair.new", args: [
+        {$: "Num", numb: BigInt(e.offsetX)},
+        {$: "Num", numb: BigInt(e.offsetY)},
+      ]};
+      this.run_event("mouse_down", [button, mouse_pos]);
+    });
+    
+    // mouse_up event
+    document.body.addEventListener("mouseup", e => {
+      var button = {$: "Num", numb: BigInt(e.button)};
+      var mouse_pos = {$: "Ctr", name: "Pair.new", args: [
+        {$: "Num", numb: BigInt(e.offsetX)},
+        {$: "Num", numb: BigInt(e.offsetY)},
+      ]};
+      this.run_event("mouse_up", [button, mouse_pos]);
+    });
 
-// Renders a JSON term to a React element
-// render (rt: Runtime) (term: JSON) : ReactElement
-function render(rt, term) {
-  switch (term.$) {
-    case "Ctr": {
-      if (term.name === "App.DOM.node") {
-        var tag = rt.string(term.args[0]);
-        var props = {};
-        for (var prop of rt.list(term.args[1])) {
-          props[rt.string(prop.args[0])] = rt.string(prop.args[1]);
+    // mouse_move event
+    document.body.addEventListener("mousemove", e => {
+      var element = this.runtime.unstring(e.target.id || "");
+      var mouse_pos = {$: "Ctr", name: "Pair.new", args: [
+        {$: "Num", numb: BigInt(e.offsetX)},
+        {$: "Num", numb: BigInt(e.offsetY)},
+      ]};
+      //this.run_event("mouse_move", [element, mouse_pos]);
+    });
+
+    // mouse_click event
+    document.body.addEventListener("click", e => {
+      var element = this.runtime.unstring(e.target.id || "");
+      this.run_event("mouse_click", [element]);
+    });
+
+  }
+
+  async componentWillUnmount() {
+    clearInterval(this.timer);
+  }
+
+  // Runs the app's draw() function on the Runtime
+  draw() {
+    if (!this.runtime) {
+      return <div>Loading...</div>;
+    }
+    var host = this.runtime.alloc_fun(this.MAIN_DRAW, [this.state.root]);
+    this.runtime.reduce(host);
+    var term = this.runtime.at(host);
+    if ( this.runtime.get_tag(term) === this.runtime.CTR
+      && this.runtime.get_ext(term) === this.PAIR_NEW) {
+      var state = this.runtime.get_loc(term, 0n);
+      var elems = this.runtime.get_loc(term, 1n);
+      this.runtime.normalize(elems);
+      var nodes = this.runtime.readback(elems);
+      this.state.root = this.runtime.at(state);
+      this.runtime.clear(host, 1n);
+      this.runtime.clear(this.runtime.get_loc(term, 0n), 2n);
+      return this.render_term(nodes);
+    }
+    throw "App draw function didn't return the expected type.";
+  };
+
+  // Renders a JSON term to a React element
+  // render (term: JSON) : ReactElement
+  render_term(term) {
+    switch (term.$) {
+      case "Ctr": {
+        if (term.name === "App.DOM.node") {
+          var tag = this.runtime.string(term.args[0]);
+          var props = {};
+          for (var prop of this.runtime.list(term.args[1])) {
+            props[this.runtime.string(prop.args[0])] = this.runtime.string(prop.args[1]);
+          }
+          props.style = {};
+          for (var style of this.runtime.list(term.args[2])) {
+            props.style[this.runtime.string(style.args[0])] = this.runtime.string(style.args[1]);
+          }
+          var children = [];
+          for (var child of this.runtime.list(term.args[3])) {
+            children.push(this.render_term(child));
+          }
+          if (term.tag === "input" || term.tag === "textarea") {
+            props.onInput = e => {
+              var elem = this.runtime.unstring(e.target.id || "");
+              var text = this.runtime.unstring(e.target.value || "");
+              this.run_event("input", [elem, text]);
+            };
+          }
+          return React.createElement(tag, props, children);
         }
-        props.style = {};
-        for (var style of rt.list(term.args[2])) {
-          props.style[rt.string(style.args[0])] = rt.string(style.args[1]);
+        if (term.name === "App.DOM.text") {
+          return this.runtime.string(term.args[0]);
         }
-        var children = [];
-        for (var child of rt.list(term.args[3])) {
-          children.push(render(rt, child));
-        }
-        return React.createElement(tag, props, children);
-      }
-      if (term.name === "App.DOM.text") {
-        return rt.string(term.args[0]);
       }
     }
   }
-}
 
-// Creates the 'Main.init' function on HVM. Returns the pointer.
-function init(rt) {
-  return rt.create_fun(rt.get_id("Main.init"), []);
-}
-
-// Creates and runs the `Main.draw` function, applied to the app's state (a
-// Ptr), on HVM. Returns the new state pointer (since the old one was consumed
-// and cloned) and the resulting renderized React element.
-function draw(rt, state) {
-  var host = rt.alloc_fun(rt.get_id("Main.draw"), [state]);
-  rt.reduce(host);
-  var term = rt.at(host);
-  if ( rt.get_tag(term) === rt.CTR
-    && rt.get_ext(term) === rt.get_id("Pair.new")) {
-    var state = rt.get_loc(term, 0n);
-    var elems = rt.get_loc(term, 1n);
-    rt.normalize(elems);
-    var nodes = rt.readback(elems);
-    rt.clear(host, 1n);
-    return [rt.at(state), render(rt, nodes)];
-  }
-  throw "App draw function didn't return the expected type.";
-};
-
-// Creates and runs the `Main.when` function, applied to an event and the app's
-// state (both Ptrs). Runs the resulting IO. Returns the next state (a Ptr).
-async function when(rt, Event, state) {
-  var host = rt.alloc_fun(rt.get_id("Main.when"), [Event, state])
-  //console.log("when", host, rt.show(host));
-  var next_state = await rt.run_io(host);
-  //console.log("next_state", rt.get_tag(next_state), rt.NUM, rt.readback(next_state));
-  return next_state;
-}
-
-function App({code}) {
-  // HVM's runtime is loaded here
-  const [rt, setRt] = useState(null);
-
-  // Stores this App's state as a ptr to an HVM node
-  const stateTuple = useState(null);
-
-  // Initializes the HVM runtime with the given app code
-  useEffect(() => {
-    async function init_runtime() {
-      var rt = await hvm((code || DEFAULT_CODE) + EXTRA_CODE);
-      setRt(rt);
-      stateTuple[1](init(rt));
-    };
-    init_runtime();
-  }, []);
-
-  // Frame event
-  useEffect(() => {
-    var timer = setInterval(async () => {
-      if (stateTuple[0]) {
-        let Event = rt.build_term({
-          $: "Ctr",
-          name: "App.Event.frame",
-          args: [],
-        });
-        stateTuple[1](await when(rt, Event, stateTuple[0]));
-      }
-    }, 1000);
-    return () => clearInterval(timer);
-  }, [stateTuple[0]]);
-
-  if (stateTuple[0] === null) {
-    return <div></div>;
+  // Runs an App.Event on HVM, updates the state root.
+  async run_event(name, args) {
+    if (!this.runtime) {
+      return;
+    }
+    var evnt = this.runtime.build_term({$: "Ctr", name: "App.Event." + name, args: args});
+    var host = this.runtime.alloc_fun(this.MAIN_WHEN, [evnt, this.state.root])
+    this.state.root = await this.runtime.run_io(host);
+    this.forceUpdate();
   }
 
-  // The draw function clones the state, so it
-  // gets a new pointer. We update it here.
-  let [new_state, drawn] = draw(rt, stateTuple[0]);
-  stateTuple[0] = new_state;
-
-  return (
-    <div className="App">
-      {drawn}
-    </div>
-  );
-      //{draw(rt, state)}
+  render() {
+    return (
+      <div className="App">
+        {this.draw()}
+      </div>
+    );
+  }
 }
+
+const EXTRA_CODE = APP_MAIN => `
+  // App.new accessors
+  (App.init (App.new i d w)) = i
+  (App.draw (App.new i d w)) = d
+  (App.when (App.new i d w)) = w
+
+  // Main app methods
+  (Main.init) = (App.init ${APP_MAIN})
+  (Main.draw s) = (Pair.new s ((App.draw ${APP_MAIN}) s))
+  (Main.when e s) = ((App.when ${APP_MAIN}) e s)
+
+  // HVM needs this to define arities
+  Load = 
+    let lib = End
+    let lib = (Import lib App.Event.init)
+    let lib = (Import lib App.Event.frame)
+    let lib = (Import lib λcode (App.Event.key_down code))
+    let lib = (Import lib λcode (App.Event.key_up code))
+    let lib = (Import lib λbutton λmouse_pos (App.Event.mouse_down button mouse_pos))
+    let lib = (Import lib λbutton λmouse_pos (App.Event.mouse_up button mouse_pos))
+    let lib = (Import lib λelement λmouse_pos (App.Event.move element mouse_pos))
+    let lib = (Import lib λelement (App.Event.mouse_click element))
+    let lib = (Import lib λelement λtext (App.Event.input element text))
+    let lib = (Import lib λvalue (Maybe.some value))
+    let lib = (Import lib Maybe.none)
+    let lib = (Import lib λfst λsnd (Pair.new fst snd))
+    let lib = (Import lib String.nil)
+    let lib = (Import lib λhead λtail (String.cons head tail))
+    lib
+`;
 
 export default App;
