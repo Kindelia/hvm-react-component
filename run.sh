f8b04cd938381921e1da6da783385a7d27097591
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-09-08 18:50:34 -0300

    improve script

--- run.sh
+++ run.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
-pwd=$(dirname $0)/..
+pwd="$(dirname "$0")/.."
 [[ -n $1 ]] && file=$1 || file=Apps/Demo/_
-cd $pwd/Wikind
+wikind=$pwd/Wikind
+hrc=$pwd/hvm-react-component
+cd "$wikind" || exit
 
 modify() {
-kind2 to-hvm $file.kind2 > $pwd/$(basename $file).hvm
-sed -n '1,8p' $pwd/hvm-react-component/src/index-demo.js > $pwd/hvm-react-component/src/index.js
-cat $pwd/$(basename $file).hvm >> $pwd/hvm-react-component/src/index.js
-sed -n '62,65p' $pwd/hvm-react-component/src/index-demo.js >> $pwd/hvm-react-component/src/index.js
+kind2 to-hvm $file.kind2 > "$hrc/hvm/$(basename $file)".hvm
+sed -n '1,8p' "$hrc/src/index-demo.js" > "$hrc/src/index.js"
+cat "$hrc/hvm/$(basename $file).hvm" >> "$hrc/src/index.js"
+sed -n '62,65p' "$hrc/src/index-demo.js" >> "$hrc/src/index.js"
 }
 
 
 while true ;do
-watch -d -t -g ls -lR $pwd/Wikind/$file.kind2  && modify
+watch -d -t -g ls -lR "$pwd/Wikind/$file.kind2"  && modify
 done
 
 # kill with `killall run.sh` on another terminal
