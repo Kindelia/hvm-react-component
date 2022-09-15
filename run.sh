9d5c4e2eaedfa08f71f4a5f97c2d6b21d7b20832
Gabriel Del <gabrieldelc.barros@gmail.com>
2022-09-15 17:23:56 -0300

    script with getopts

--- run.sh
+++ run.sh
@@ -1,21 +1,51 @@
 #!/bin/bash
 
+header="import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+const main = 'Apps.Demo';
+const code = \`"
+
+footer="\`;
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App main={main} code={code} />);"
+
+help(){
+  echo "-f: file #Default: Apps/Demo/_
+  -1: run one time
+  -s: start develop mode, build at every modification
+  -q: finish develop mode #run on another terminal"
+}
+
+modify() {
+[[ -z $file ]] && file=Apps/Demo/_
+kind2 to-hvm $file.kind2 &> "$hrc/hvm/$(basename $file)".hvm
+echo "$header" > "$hrc/src/index.js"
+cat "$hrc/hvm/$(basename $file).hvm" >> "$hrc/src/index.js"
+echo "$footer" >> "$hrc/src/index.js"
+ }
+
 pwd="$(dirname "$0")/.."
-[[ -n $1 ]] && file=$1 || file=Apps/Demo/_
 wikind=$pwd/Wikind
 hrc=$pwd/hvm-react-component
 cd "$wikind" || exit
 
-modify() {
-kind2 to-hvm $file.kind2 > "$hrc/hvm/$(basename $file)".hvm
-sed -n '1,8p' "$hrc/src/index-demo.js" > "$hrc/src/index.js"
-cat "$hrc/hvm/$(basename $file).hvm" >> "$hrc/src/index.js"
-sed -n '62,65p' "$hrc/src/index-demo.js" >> "$hrc/src/index.js"
-}
-
-
-while true ;do
-watch -d -t -g ls -lR "$pwd/Wikind/$file.kind2"  && modify
+while getopts "hf:1sq" OPT; do
+case "$OPT" in
+"h") help;;
+"f") file=$OPTARG;;
+"1") modify;;
+"s")
+  while true ;do
+  watch -d -t -g ls -lR "$pwd/Wikind/$file.kind2"  && modify
+  done
+  ;;
+"q") killall "$(basename $0)";;
+esac
 done
+
+[[ -z "$1" ]] && help
 
 # kill with `killall run.sh` on another terminal
